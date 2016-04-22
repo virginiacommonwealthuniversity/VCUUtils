d9d06699520ce930b6115abcfcdb885de2a98606
Joel R Eisner <jeisner93@gmail.com>
2016-04-22 08:32:38 -0400

    Added the todaysDate member to the root module

--- src/7.4/VCUUtils.js
+++ src/7.4/VCUUtils.js
@@ -18,6 +18,22 @@ var VCUUtils = (function(utils) {
     */
     utils.version = '2.0.0_{{date-string}}';
 
+    /**
+    * Todays date as a formatted date string
+    * @member todaysDate
+    * @returns {string} a date string (i.e. Friday, April 22, 2016)
+    * @example
+    * VCUUtils.todaysDate;
+    */
+    utils.todaysDate = (function() {
+        var now = new Date(),
+            days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            date = ((now.getDate() < 10) ? "0" : "") + now.getDate(),
+            today = days[now.getDay()] + ", " + months[now.getMonth()] + " " + date + ", " + now.getFullYear();
+        return today;
+    })();
+
     return utils;
 
 })(VCUUtils || {});
