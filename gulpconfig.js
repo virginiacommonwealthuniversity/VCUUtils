ad1d7b44977990a730612642c4f5b33024b42062
Joel Eisner <jeisner93@gmail.com>
2016-06-13 10:37:11 -0400

    Updated gulpconfig.js to include the new 'sort' module

--- gulpconfig.js
+++ gulpconfig.js
@@ -5,7 +5,8 @@ module.exports = {
         input: [
             './src/7.4/VCUUtils.js',
             './src/7.4/components/parseInj.js',
-            './src/7.4/components/parseTemp.js'
+            './src/7.4/components/parseTemp.js',
+            './src/7.4/components/sort.js'
         ],
         filename: {
             expanded: 'VCUUtils-7.4.js',
