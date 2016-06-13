30d5bf38dcd34a2c00eee3472654ada8a7b4735b
Joel Eisner <jeisner93@gmail.com>
2016-06-13 10:37:50 -0400

    Rebuilt the 7.4 VCUUtils library for the 2.3.0 addition of the 'sort' module

--- dist/7.4/VCUUtils-7.4.js
+++ dist/7.4/VCUUtils-7.4.js
@@ -16,7 +16,7 @@ var VCUUtils = (function(utils) {
     * @example
     * VCUUtils.version;
     */
-    utils.version = '2.1.2_06.01.2016';
+    utils.version = '2.1.2_06.13.2016';
 
     /**
     * Todays date as a formatted date string
@@ -245,4 +245,66 @@ VCUUtils.parseTemp.jade = function (str) {
     } else {
         return output;
     }
+};
+
+/**
+* sort - The Sort Module
+* @namespace sort
+* @extends VCUUtils
+* @author Joel Eisner <eisnerjr@vcu.edu>
+* @version 1.0.0
+* @example
+* VCUUtils.sort
+*/
+/* jshint strict: false */
+VCUUtils.sort = VCUUtils.sort || {};
+
+/**
+* Alphebetically bubble-sorts an array via an object key
+* @function sort.array.viaObjectKey
+* @param {array} array - The array you'd like to sort
+* @param {string} parameter - The object key you'd like to sort by
+* @returns {array} an alphebetically sorted array
+* @example
+* VCUUtils.sort.array.viaObjectKey(array, string);
+*/
+VCUUtils.sort.array.viaObjectKey = function (array, parameter) {
+    // Empty swapped variable
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < array.length - 1; i++) {
+            if (array[i][parameter] > array[i + 1][parameter]) {
+                var temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+};
+
+/**
+* Reverse-alphebetically bubble-sorts an array via an object key
+* @function sort.array.viaObjectKey
+* @param {array} array - The array you'd like to sort
+* @param {string} parameter - The object key you'd like to sort by
+* @returns {array} an alphebetically sorted array
+* @example
+* VCUUtils.sort.array.viaObjectKey.reverse(array, string);
+*/
+VCUUtils.sort.array.viaObjectKey.reverse = function (array, parameter) {
+    // Empty swapped variable
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < array.length - 1; i++) {
+            if (array[i][parameter] < array[i + 1][parameter]) {
+                var temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
 };
