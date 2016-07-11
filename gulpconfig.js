b4935b7f0564d945b094fb6aae65be136d2b1807
Joel Eisner <jeisner93@gmail.com>
2016-07-11 14:50:27 -0400

    --ammend

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,11 +1,10 @@
-'use strict';
-
 module.exports = {
     v74: {
         input: [
             './src/7.4/VCUUtils.js',
             './src/7.4/components/parseInj.js',
             './src/7.4/components/parseTemp.js',
+            './src/7.4/components/siteConfig.js',
             './src/7.4/components/sort.js'
         ],
         filename: {
@@ -14,4 +13,4 @@ module.exports = {
         },
         output: './dist/7.4'
     }
-}
+};
