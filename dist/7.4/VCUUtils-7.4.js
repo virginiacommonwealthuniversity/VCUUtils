8fdae7a1a46d2becce52680bed5b69a97cde59ca
Joel Eisner <jeisner93@gmail.com>
2016-07-19 13:35:14 -0400

    Recompiled the library to remove the siteConfig module

--- dist/7.4/VCUUtils-7.4.js
+++ dist/7.4/VCUUtils-7.4.js
@@ -5,7 +5,7 @@
 * VCUUtils
 * @module
 * @author Joel Eisner <eisnerjr@vcu.edu>
-* @version 2.4.0
+* @version 2.4.1
 */
 var VCUUtils = (function(utils) {
 
@@ -16,7 +16,7 @@ var VCUUtils = (function(utils) {
     * @example
     * VCUUtils.version;
     */
-    utils.version = '2.4.0_07.11.2016';
+    utils.version = '2.4.1_07.19.2016';
 
     /**
     * Todays date as a formatted date string
@@ -244,34 +244,6 @@ VCUUtils.parseTemp.jade = function (str) {
         return output;
     } else {
         return output;
-    }
-};
-
-/**
-* siteConfig - The Site Config Module
-* @namespace siteConfig
-* @extends VCUUtils
-* @author Joel Eisner <eisnerjr@vcu.edu>
-* @version 1.0.0
-* @example
-* VCUUtils.siteConfig
-*/
-/* jshint strict: false */
-VCUUtils.siteConfig = VCUUtils.siteConfig || {};
-
-/**
-* Matches a string against a regex statement, and if true, returns the value between a beginning/ending injector
-* @function siteConfig.nameSpaceExists
-* @param {object} object - The object you are looking for via namespace
-* @returns {boolean} whether the namespace you are looking for exists or not
-* @example
-* VCUUtils.siteConfig.nameSpaceExists(SiteConfig.plugins.test);
-*/
-VCUUtils.siteConfig.nameSpaceExists = function (object) {
-    if (typeof obj !== 'undefined') {
-        return true;
-    } else {
-        return false;
     }
 };
 
